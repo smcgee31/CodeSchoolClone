7f00086d3d473f3e763edad2391e5201bb68d4d7
smcgee31 <solodesign31@gmail.com>
2016-06-16 01:07:35 -0600

    trying to get this stupid thing to work on a MEAN server

--- server/index.js
+++ server/index.js
@@ -1,23 +1,7 @@
-// Require dependencies
 var express = require('express');
-var bodyParser = require('body-parser');
-
-
-// initilize app
 var app = express();
-
-app.use(express.static(__dirname + './../public'));
-
-// initilize dependencies
-app.use(bodyParser.json());
-
-
-// Variables
+app.use(express.static(__dirname + '/public'));
 var port = 6000;
-var mongoURI = 'mongodb://localhost:27017/DBName';
-
-// MongoDB connection
-// App Listen
 app.listen(port, function() {
     console.log('listening on port ', port);
 });
