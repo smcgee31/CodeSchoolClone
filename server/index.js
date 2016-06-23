cd1f074b79ef3218c037f8ff72c8db1b3ff53f9f
smcgee31 <solodesign31@gmail.com>
2016-06-23 11:22:03 -0600

    still for hosting

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
+// CodeSchoolClone
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public'));
 var port = 6000;
 app.listen(port, function() {
     console.log('listening on port ', port);
