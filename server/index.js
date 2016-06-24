3dc05540692f900af99ba1b5402f7c781a01f5bc
smcgee31 <solodesign31@gmail.com>
2016-06-24 11:03:28 -0600

    changes for hosting

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 // CodeSchoolClone
-var express = require('express');
+var express = require( 'express' );
 var app = express();
 var port = 6000;
-app.listen(port, function() {
-    console.log('listening on port ', port);
-});
+
+app.use( express.static( __dirname + '/../public') );
+
+app.listen( port, function () {
+	console.log( 'listening on port ', port );
+} );
